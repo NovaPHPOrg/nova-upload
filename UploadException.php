c55f7af56681ad1e9fc212961f918f063a8f9994
ankio <ankio@ankio.net>
2024-10-09 18:05:20 +0800

    :bug: (exception): 使用 Throwable 替换 Exception
    将 Exception 替换为 Throwable，以捕获更广泛的异常类型。

--- UploadException.php
+++ UploadException.php
@@ -2,11 +2,13 @@
 
 namespace nova\plugin\upload;
 
+use Exception;
 use nova\framework\log\Logger;
+use Throwable;
 
-class UploadException extends \Exception
+class UploadException extends Exception
 {
-    public function __construct($message, $code = 0, \Throwable $previous = null)
+    public function __construct($message, $code = 0, Throwable $previous = null)
     {
         Logger::error("UploadException: $message");
         parent::__construct($message, $code, $previous);
